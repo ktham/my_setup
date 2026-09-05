5cc6ee733a5e28fe3dae0993d2ab3aea599a6dea
Kevin Tham <ktham@users.noreply.github.com>
2026-09-05 10:26:47 -0700

    build(deps): bump nixpkgs, home-manager and nix-darwin to 26.05 (#21)

--- flake.nix
+++ flake.nix
@@ -2,16 +2,16 @@
   description = "NixOS configuration used by ktham";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
